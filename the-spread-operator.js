293d3c8c74a6fb22b15e146c43938ec8cafb99e5
Cam Wheatley <cameron_2000@live.co.uk>
2024-03-21 11:26:49 +0000

    inital commit

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -10,3 +10,23 @@
 // Copying an object
 
 // Copying only part of an array/object
+
+let arr1 = [1, 2, 3, 4];
+console.log(arr1);
+
+let arr2 = [...arr1, 7];
+console.log(arr2);
+
+let camWheatley = {
+    fName: 'Cam',
+    lName: 'Wheatley',
+    girlfriend: 'Hannah',
+    datesWorked: [16, 17, 18, 19, 20, 21]
+}
+
+camWheatley.datesWorked = camWheatley.datesWorked.concat(23);
+
+
+let camSpread = [...camWheatley.datesWorked];
+console.log(camWheatley.datesWorked);
+console.log(camSpread)
